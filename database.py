8a926efb9636e87b2797aa3c2645d20d55de9867
yusufyilmazisep <1232296@isep.ipp.pt>
2025-01-07 15:17:02 +0300

    veri tabanı bağlantısı sağlandı ve test edildi.

--- database.py
+++ database.py
@@ -5,11 +5,30 @@ def create_connection():
     try:
         conn = psycopg2.connect(
             host="localhost",
-            database="your_database",
-            user="your_username",
-            password="your_password"
+            database="greensaverdb",
+            user="postgres",
+            password="1234"
         )
         return conn
     except Exception as e:
         print(f"Database connection failed: {e}")
         return None
+
+def test_connection():
+    conn = create_connection()
+    if conn:  # Bağlantı başarılıysa
+        try:
+            cursor = conn.cursor()
+            # PostgreSQL versiyon sorgusu
+            cursor.execute("SELECT version();")
+            result = cursor.fetchone()
+            print("PostgreSQL version:", result[0])  # Versiyon bilgisi yazdırılır
+        except Exception as e:
+            print(f"Query failed: {e}")
+        finally:
+            conn.close()
+            print("Connection closed.")
+    else:
+        print("Failed to connect to the database.")
+
+test_connection()
